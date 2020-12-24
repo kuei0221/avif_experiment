501c7eacf043e4db6657188a0d64908d6e4f3884
michaelhwang <michaelhwang0619@gmail.com>
2020-12-24 18:17:35 +0800

    feat(model): update product with avif setting - photo will first resizing with default processor - them compress to avif form with avif processor

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
-  has_attached_file :photo, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { avif: [:avif] }, processors: [:default, :avif], default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+
+  before_save :floor_process_time
+
+  private
+
+  def floor_process_time
+    return unless process_time
+
+    self.process_time = process_time.floor(2)
+  end
 end
